4b764e5f9fc462aa6f753434fe1b137db972ba9c
thinh <thinhqn999@gmail.com>
2024-12-12 00:08:47 +0700

    tạo folder Services chứa các interface service sẽ sử dụng và khai báo interface IAuthApi

---
+++ ThiTracNghiemTrucTuyen.Web/Services/IAuthApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using ThiTracNghiemTrucTuyen.Shared.DTOs;
+
+namespace ThiTracNghiemTrucTuyen.Web.Services
+{
+  public interface IAuthApi
+  {
+    [Post("/api/auth/login")]
+    Task<AuthensResponseDto> LoginAsync(LoginDto loginDto);
+  }
+}
