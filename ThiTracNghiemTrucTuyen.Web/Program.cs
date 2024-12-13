1705c991e0ed436c4959973bab32da2e102ba0c1
thinh <thinhqn999@gmail.com>
2024-12-13 16:43:33 +0700

    uri url urn + note6 + hàm không trả về ConfigureRefit

--- ThiTracNghiemTrucTuyen.Web/Program.cs
+++ ThiTracNghiemTrucTuyen.Web/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Refit;
 using ThiTracNghiemTrucTuyen.Web;
+using ThiTracNghiemTrucTuyen.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+static void ConfigureRefit(IServiceCollection services)
+{
+  const string ApiUrl = "https://localhost:7274";
+  services.AddRefitClient<IAuthApi>().ConfigureHttpClient(httpClient => httpClient.BaseAddress = new Uri(ApiUrl));
+}
